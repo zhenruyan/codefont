840b459c03220f5f76dc0899a183b4186d968cbc
zhenruyan <1263580126@qq.com>
2017-08-30 21:52:56 +0800

    windows

--- windosinstall.bat
+++ windosinstall.bat
@@ -1,7 +1,11 @@
-@echo off
+color 0A
+title fonts install Powered by freeidea
+setlocal ENABLEEXTENSIONS
 set exs=*.otf *.ttf
-cd/d "lib/"
-for /r %%i in (%exs%) do (
-  echo %%i
-  xcopy/i "%%i" "c:/fonts">nul)
+cd/d "lib"
+for /r %%a in (%exs%) do (
+dir %windir%\fonts\%%a>nul 2>nul||(copy %%a %windir%\fonts>nul 2>nul&rundll32.exe gdi32.dll,AddFontResourceA %windir%\fonts\%%a)
+)
+
 pause
+
